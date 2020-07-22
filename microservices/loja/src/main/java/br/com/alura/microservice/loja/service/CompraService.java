37be62cdafae3d7642a8f83915258724b6ff099e
Gabriel Alan <alang.von@gmail.com>
2020-07-22 08:02:37 -0300

    Fornecedor is back.

--- microservices/loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ microservices/loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 import br.com.alura.microservice.loja.client.FornecedorClient;
 import br.com.alura.microservice.loja.dto.CompraDTO;
 import br.com.alura.microservice.loja.dto.InfoFornecedorDTO;
+import br.com.alura.microservice.loja.dto.InfoPedidoDTO;
+import br.com.alura.microservice.loja.model.Compra;
 
 @Service
 public class CompraService {
@@ -13,15 +15,18 @@ public class CompraService {
 	@Autowired
 	private FornecedorClient fornecedorClient;
 	
-	public String realizaCompra(CompraDTO compra) {
+	public Compra realizaCompra(CompraDTO compra) {
 
 		InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
 		
-		if(info != null) {
-			return info.getEndereco();
-		} else {
-			return "Não encontrado";
-		}
+		InfoPedidoDTO pedido = fornecedorClient.realizaPedido(compra.getItens());
+		
+		Compra compraSalva = new Compra();
+		compraSalva.setPedidoId(pedido.getId());
+		compraSalva.setTempoDePreparo(pedido.getTempoDePreparo());
+		compraSalva.setEnderecoDestino(compra.getEndereco().toString());
+		
+		return compraSalva;
 	}
 
 }
