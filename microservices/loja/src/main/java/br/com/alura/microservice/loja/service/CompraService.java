8b5b8a02eb6885ebf831eea51e0133aa97d71b67
Gabriel Alan <alang.von@gmail.com>
2020-07-27 23:59:05 -0300

    Logs, PaperTrail & Spring Sleuth(Trace Id).

--- microservices/loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ microservices/loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -1,5 +1,7 @@
 package br.com.alura.microservice.loja.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,13 +14,18 @@ import br.com.alura.microservice.loja.model.Compra;
 @Service
 public class CompraService {
 
+	private static final Logger LOG = LoggerFactory.getLogger(CompraService.class);
+
 	@Autowired
 	private FornecedorClient fornecedorClient;
 	
 	public Compra realizaCompra(CompraDTO compra) {
 
+		
+		LOG.info("Buscando informações do fornecedor de {}" , compra.getEndereco().getEstado());
 		InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
 		
+		LOG.info("Realizando um pedido!");
 		InfoPedidoDTO pedido = fornecedorClient.realizaPedido(compra.getItens());
 		
 		Compra compraSalva = new Compra();
